4845cfd5607efc23452110a4bfb3ba4bf4878cbe
irinka-irinka <80964268+irinka-irinka@users.noreply.github.com>
2021-03-19 16:30:33 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function () {
+    $('#app').vide({
+        mp4: 'video/video.mp4',
+        webm:'video/video.webm',
+        ogv: 'video/video.ogv',
+        poster:  'img/coveimage.jpg'
+    });
+
+});
+
